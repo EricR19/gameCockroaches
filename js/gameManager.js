9d13fcab83a05a34278726d21cb54fb11599c88c
Eric Ruiz <45463829+EricR19@users.noreply.github.com>
2025-04-03 19:53:41 -0600

    Update gameManager.js

--- js/gameManager.js
+++ js/gameManager.js
@@ -1,47 +1,21 @@
-import { CardsManager } from "./cardsManager.js";
-import { Game } from "./game.js";
-import { Stats } from "./libs/stats.js";
-
 export class GameManager {
+  static instance = null;
+
   constructor() {
     if (GameManager.instance) {
-      return GameManager.instance;
-    } else {
-      GameManager.instance = this;
+      return GameManager.instance; // Asegurarse de que solo haya una instancia
     }
 
-    this.width = window.outerWidth;
-    this.height = window.outerHeight;
-    this.cardsManager = new CardsManager();
-    this.game = new Game(this.width, this.height);
+    GameManager.instance = this;
 
-    let isShowingStats = false;
-    if (isShowingStats) {
-      this.stats = new Stats();
-      this.stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
-      document.body.appendChild(this.stats.dom);
+    // Configuración global del juego
+    this.width = 800; // Ancho del juego
+    this.height = 600; // Alto del juego
+    this.backgroundColor = "white"; // Color de fondo
+  }
 
-      this.game.events.on("step", (time, delta) => {
-        this.stats.begin();
-      });
-
-      this.game.events.on("poststep", (time, delta) => {
-        this.stats.end();
-      });
-    }
+  // Método para obtener el tamaño del juego
+  getGameSize() {
+    return { width: this.width, height: this.height };
   }
 }
-
-// scene.transition({
-//     target: target,
-//     duration: 100
-// });
-
-// export const LOADER_SCENE = 'LoaderScene';
-// export const MENU_SCENE = 'MenuScene';
-// export const PLAY_SCENE = 'PlayScene';
-// export const END_SCENE = 'EndScene';
-// export const SETTINGS_SCENE = 'SettingsScene';
-// export const DRAWING_SELECTION_SCENE = 'DrawingSelectionScene';
-// export const PALLETE_SELECTION_SCENE = 'PalleteSelectionScene';
-// export const DIFFICULTY_SCENE = 'DifficultyScene';
